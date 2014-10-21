6f78ef7241411fa4f17501316e27e5436dc15ee5
Abigail Stevens <A.L.Stevens@uva.nl>
2014-10-21 18:16:22 +0200

    Gets exposure time from the .fak spectra files, opens the plot with ImageJ, uses command line flags.

--- run_simulate.sh
+++ run_simulate.sh
@@ -8,11 +8,23 @@ out_dir="$exe_dir/out_sim"
 freq=401.0
 bb_spec="spectra/fakeit_mean.fak"
 pl_spec="spectra/fakeit_mean.fak"
-exposure=2
-phase_ci=-1.0  # Negative means ci lags ref, positive means ref lags ci
+amp_ci=0.09
+amp_ref=0.09
+exposure=1.0
+phase_spec=-1.0
 
+if [ ${bb_spec: -4} == ".fak" ] && [ ${pl_spec: -4} == ".fak" ]; then
+	bb_exposure=$(python -c "from tools import get_key_val; print get_key_val('$bb_spec', 1, 'EXPOSURE')")
+	pl_exposure=$(python -c "from tools import get_key_val; print get_key_val('$pl_spec', 1, 'EXPOSURE')")
+	if [ $bb_exposure == $pl_exposure ]; then
+		exposure=$bb_exposure
+	fi
+fi
 
+# python "$exe_dir"/simulate_lightcurves.py -h
 
-python "$exe_dir"/simulate_lightcurves.py -h
+python "$exe_dir"/simulate_lightcurves.py --freq "$freq" --bb "$bb_spec" --pl "$pl_spec" --amp_ci "$amp_ci" --amp_ref "$amp_ref" --exposure "$exposure" --phase_spec "$phase_spec"
 
-python "$exe_dir"/simulate_lightcurves.py --freq "$freq" --bb "$bb_spec" --pl "$pl_spec" --exposure "$exposure" --phase_ci "$phase_ci"
+if [ -e "$exe_dir/plot.png" ]; then
+	open -a ImageJ "$exe_dir/plot.png"
+fi
