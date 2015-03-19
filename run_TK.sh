20a1df2174b39fd662a2661514628023516fcc94
Abigail Stevens <A.L.Stevens@uva.nl>
2015-03-19 18:13:34 +0100

    Using relative qpo and pl scale factors.

--- run_TK.sh
+++ run_TK.sh
@@ -12,19 +12,15 @@ prefix="FAKE-TK-GX339B"
 nbins=8192
 dt=0.0078125
 rebin_const=1.01
-meanrate=1000.0  # for one pcu
-# meanrate=0
-# numsegments=267
-numsegments=10
-# variance=4.61214030908e-07
-# variance=2.1273510648e-07
-variance=1e-6
-# variance=1
-pl_scale=3.29617761e-04
-qpo_scale=2.06908556e-02
+numsegments=267
+# numsegments=100
+exposure=17450.0
+variance=0.00731682828543  ##in frac rms units
+pl_scale=0.01  ## relative scale factor
+qpo_scale=1    ## relative scale factor
 # pl_scale=0.00033744
 # qpo_scale=0.02769814
-fake_e_spec="$out_dir/${prefix}.fak"
+fake_e_spec="$sim_dir/spectra/${prefix}.fak"
 
 
 cd "$sim_dir"
@@ -39,21 +35,20 @@ for (( i=0; i<1; i++ )); do
 	ccf_2D_plot="$out_dir/TK_ccf_2D_${i}.png"
 	
 # 	echo "$i"
-python "$sim_dir"/TimmerKoenig.py "$nbins" "$dt" "$meanrate" \
-		"$numsegments" "$variance" "$pl_scale" "$qpo_scale" "$fake_e_spec" \
-		"$pow_out" "$ccf_out"
+python "$sim_dir"/TimmerKoenig.py "$nbins" "$dt" "$numsegments" "$variance" \
+	"$exposure" "$pl_scale" "$qpo_scale" "$fake_e_spec" "$pow_out" "$ccf_out"
 
 	python "$pow_dir"/plot_powerspec.py "$pow_out" -o "$pow_plot" -p "$prefix"
-	# if [ -e "$pow_plot" ]; then open "$pow_plot"; fi
+if [ -e "$pow_plot" ]; then open "$pow_plot"; fi
 
 	python "$pow_dir"/rebin_powerspec.py "$pow_out" "$pow_rb_out" -o "$pow_rb_plot"\
 		-p "$prefix" -c "$rebin_const"
 	if [ -e "$pow_rb_plot" ]; then open "$pow_rb_plot"; fi
 
-	if [ -e "$ccf_out" ]; then
-		python "$ccf_dir"/plot_ccf.py "$ccf_out" -o "$ccf_plot" -p "$prefix" 
-	# 	if [ -e "$ccf_plot"_chan_06.png ]; then open "$ccf_plot"_chan_06.png; fi
-		python "$ccf_dir"/plot_2d.py "$ccf_out" -o "$ccf_2D_plot" -p "${prefix}"
-		if [ -e "$ccf_2D_plot" ]; then open "$ccf_2D_plot"; fi
-	fi
+# 	if [ -e "$ccf_out" ]; then
+# 		python "$ccf_dir"/plot_ccf.py "$ccf_out" -o "$ccf_plot" -p "$prefix" 
+# 	# 	if [ -e "$ccf_plot"_chan_06.png ]; then open "$ccf_plot"_chan_06.png; fi
+# 		python "$ccf_dir"/plot_2d.py "$ccf_out" -o "$ccf_2D_plot" -p "${prefix}"
+# # 		if [ -e "$ccf_2D_plot" ]; then open "$ccf_2D_plot"; fi
+# 	fi
 done
