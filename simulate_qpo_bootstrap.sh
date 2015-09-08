6d4a77131769b7152dd187da7b443aad9164d057
Abigail Stevens <A.L.Stevens@uva.nl>
2015-09-08 20:19:19 +0200

    Not doing anything with this right now.

--- simulate_qpo_bootstrap.sh
+++ simulate_qpo_bootstrap.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+
+day = datetime.datetime.today().strftime("%y%m%d")
+prefix = "FAKE-GX339B"
+
+filename = "/Users/abigailstevens/Dropbox/Research/simulate/out_sim/" \
+        "%s_150722_nopoiss" % (prefix)
